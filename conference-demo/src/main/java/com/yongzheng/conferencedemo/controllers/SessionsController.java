8f1b0b289708ea27d3926202c9a50c0f8c30d666
Yongzheng Zhang <yongzheng9405@gmail.com>
2019-11-29 20:57:15 +0800

    Add controller for speaker and session

---
+++ conference-demo/src/main/java/com/yongzheng/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,34 @@
+package com.yongzheng.conferencedemo.controllers;
+
+import com.yongzheng.conferencedemo.models.Session;
+import com.yongzheng.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+}
---
+++ conference-demo/src/main/java/com/yongzheng/conferencedemo/controllers/SpeaksController.java
@@ -0,0 +1,30 @@
+package com.yongzheng.conferencedemo.controllers;
+
+
+import com.yongzheng.conferencedemo.models.Speaker;
+import com.yongzheng.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeaksController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+}
