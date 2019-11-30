65bf2cb2037fe40c339a41c8352cb4afe21b4d76
Yongzheng Zhang <yongzheng9405@gmail.com>
2019-11-30 10:17:10 +0800

    Serialization of data

--- conference-demo/src/main/java/com/yongzheng/conferencedemo/models/Session.java
+++ conference-demo/src/main/java/com/yongzheng/conferencedemo/models/Session.java
@@ -1,9 +1,12 @@
 package com.yongzheng.conferencedemo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "sessions")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- conference-demo/src/main/java/com/yongzheng/conferencedemo/models/Speaker.java
+++ conference-demo/src/main/java/com/yongzheng/conferencedemo/models/Speaker.java
@@ -1,11 +1,14 @@
 package com.yongzheng.conferencedemo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Speaker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +25,7 @@ public class Speaker {
     private byte[] speaker_photo;
 
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     private List<Session> sessions;
 
     public Speaker() {
